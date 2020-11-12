a9cd27cd1801f3e6bc0d0d409f41b4be88563902
Leonardo Marçal <66973637+Leonnmarshall@users.noreply.github.com>
2020-11-11 21:43:32 -0300

    Create mudando-classes-dos-vetores.r

---
+++ mudando-classes-dos-vetores.r
@@ -0,0 +1,11 @@
+#transformação de vetores
+#para transformação de classes dos vetores utiliza-se a função as.(), exemplos
+
+x <- 0:6
+class(x)
+as.numeric(x)
+as.logical(x)
+as.character(x)
+
+#espero ter ajudado
+#me siga no instagram @data_universeBR
